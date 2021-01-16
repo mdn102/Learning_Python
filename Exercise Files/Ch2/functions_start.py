469584136eadc5891181c95d3545238adaa265b7
nguyendm77 <nguyendm77@gmail.com>
2021-01-15 22:14:03 -0800

    updated code

---
+++ Exercise Files/Ch2/functions_start.py
@@ -0,0 +1,25 @@
+#
+# Example file for working with functions
+#
+
+# define a basic function
+def func1():
+    print("I am a function ")
+    
+
+# function that takes arguments
+
+
+# function that returns a value
+
+
+# function with default value for an argument
+
+
+#function with variable number of arguments
+
+func1()
+print (func1())
+print (func1)
+
+
