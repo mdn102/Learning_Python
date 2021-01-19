22f8a68940c4db69c571a5af2fffb79081c459fc
nguyendm77 <nguyendm77@gmail.com>
2021-01-19 11:52:22 -0800

    updated code

--- crawler_coding_challenge.py
+++ crawler_coding_challenge.py
@@ -1,20 +1,5 @@
-'''--->
-# We get the url
-r = requests.get("https://en.wikipedia.org/wiki/Microsoft")
-soup = BeautifulSoup(r.content)
 
-# We get the words within paragrphs
-text_p = (''.join(s.findAll(text=True))for s in soup.findAll('p'))
-c_p = Counter((x.rstrip(punctuation).lower() for y in text_p for x in y.split()))
 
-# We get the words within divs
-text_div = (''.join(s.findAll(text=True))for s in soup.findAll('div'))
-c_div = Counter((x.rstrip(punctuation).lower() for y in text_div for x in y.split()))
-
-# We sum the two countesr and get a list with words count from most to less common
-total = c_div + c_p
-list_most_common_words = total.most_common(10) 
-<---'''
 
 import requests    
 import re    
@@ -68,5 +53,5 @@ class PyCrawler(object):
         self.crawl(self.starting_url)    
 
 if __name__ == "__main__":    
-    crawler = PyCrawler("https://google.com")     
+    crawler = PyCrawler("https://en.wikipedia.org/wiki/Microsoft")
     crawler.start()
