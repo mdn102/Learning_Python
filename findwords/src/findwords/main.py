9df1275586f6325f1659373ee1131da2e1366612
nguyendm77 <nguyendm77@gmail.com>
2021-01-19 22:10:09 -0800

    updated code

--- findwords/src/findwords/main.py
+++ findwords/src/findwords/main.py
@@ -1,4 +1,14 @@
 import sys, getopt
+import urllib.request
+from urllib.request import urlopen
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+def crawler(url):
+    content = url
+    return content
+
 
 if __name__ == '__main__':
     # Parsing argument
@@ -7,3 +17,12 @@ if __name__ == '__main__':
 
     print (f"Print total {NUM_WORDS} words")
     print (f"Words that are excluded: {EXCLUDE_WORDS}")
+    
+    content = crawler("https://en.wikipedia.org/wiki/Microsoft")
+    print(content)
+    
+    # read the data from the URL and print it
+    file = urlopen(content)
+    data = file.read()
+    print(data)
+    
